072d4c0bce0d7d623ffed5bfe2f7188389fc439d
Sebastian Drewes <sebas.drewes@gmail.com>
2021-05-14 19:56:24 -0300

    agregada display libro individual, borrar libro y cambiar estado read

--- javascript.js
+++ javascript.js
@@ -4,14 +4,24 @@ const tituloLibro = document.querySelector(".tituloLibro")
 const autor = document.querySelector(".autor")
 const paginas = document.querySelector(".paginas")
 const read = document.querySelector(".read")
+const libreria = document.querySelector(".libreria")
 
 function Book(title, author, pages, read) {
     this.title = title 
-    this.author = author
-    this.pages = pages
-    this.read = read
-    Book.prototype.info = function() {
-    return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read}`
+    this.author = `de ${author}`
+    this.pages = `${pages} paginas`
+    this.read = read;
+    this.remove = function() {
+        for (let i=0; i<myLibrary.length; i++) {
+            if (myLibrary[i] === this) {
+                myLibrary.splice(i, 1)
+            }
+        }
+        displayBooks()
+    this.readStatus = function () {
+        
+    }
+    
 }}
 
 function addBookToLibrary(){
@@ -26,43 +36,50 @@ function addBookToLibrary(){
 }
 //https://stackoverflow.com/questions/39155810/displaying-an-object-within-an-array-through-dom
 function displayBooks() {
-    while (tituloLibro.firstChild) {
-        tituloLibro.removeChild(tituloLibro.firstChild);
+    //borra todos los libros anteriores para hacer todos nuevamente
+    while (libreria.firstChild) {
+        libreria.removeChild(libreria.firstChild);
     }
-    while (autor.firstChild) {
-        autor.removeChild(autor.firstChild);
-    }
-    while (paginas.firstChild) {
-        paginas.removeChild(paginas.firstChild);
-    }
-    while (read.firstChild) {
-        read.removeChild(read.firstChild);
-    }
-    
     for (let i=0; i<myLibrary.length; i++) {
-        const novoTitulo= document.createElement('div');
-        novoTitulo.classList.add("libro");
-        novoTitulo.textContent += myLibrary[i].title;
-        tituloLibro.appendChild(novoTitulo);
-
-        const novoAutor= document.createElement('div');
-        novoAutor.classList.add("libro");
-        novoAutor.textContent += myLibrary[i].author;
-        autor.appendChild(novoAutor);
-
-        const novoPaginas= document.createElement('div');
-        novoPaginas.classList.add("libro");
-        novoPaginas.textContent += myLibrary[i].pages;
-        paginas.appendChild(novoPaginas);
-
-        const novoLeido= document.createElement('div');
-        novoLeido.classList.add("libro");
-        novoLeido.textContent += myLibrary[i].read;
-        read.appendChild(novoLeido);
+        const libroAgregado= document.createElement('div');
+        libroAgregado.classList.add("libroAgregado");
+        const tituloAgregado= document.createElement('div');
+        tituloAgregado.classList.add("tituloAgregado");
+        tituloAgregado.textContent = myLibrary[i].title;
+        const autorAgregado= document.createElement('div');
+        autorAgregado.classList.add("autorAgregado");
+        autorAgregado.textContent = myLibrary[i].author;
+        const paginasAgregado= document.createElement('div');
+        paginasAgregado.classList.add("paginasAgregado");
+        paginasAgregado.textContent = myLibrary[i].pages;
+        const readAgregado= document.createElement('div');
+        readAgregado.classList.add("readAgregado");
+        readAgregado.textContent = myLibrary[i].read;
+        readAgregado.addEventListener('click', () => {
+            if (myLibrary[i].read === "Leido") {
+                myLibrary[i].read = "No Leido"
+            }else {
+                myLibrary[i].read = "Leido"
+            }
+            readAgregado.textContent = myLibrary[i].read;
+        })
+        const removeAgregado= document.createElement('button');
+        removeAgregado.classList.add("removeAgregado");
+        removeAgregado.addEventListener('click', () => {
+            myLibrary[i].remove();
+        })
+        libreria.appendChild(libroAgregado);
+        libroAgregado.appendChild(tituloAgregado);
+        libroAgregado.appendChild(autorAgregado);
+        libroAgregado.appendChild(paginasAgregado);
+        libroAgregado.appendChild(readAgregado);
+        libroAgregado.appendChild(removeAgregado);
+        
     }
 }
-const theHobbit = new Book("The Hobbit", "J.R.R Tolkien", 295, "not read yet")
-let myLibrary = [theHobbit];
+const theHobbit = new Book("The Hobbit", "J.R.R Tolkien", 295, "Leido")
+const harryPotter = new Book("Harry Potter", "J.K Rollin", 255, "Leido")
+let myLibrary = [theHobbit, harryPotter];
 displayBooks();
 nuevoLibro.addEventListener('click', () => {
     addBookToLibrary()
