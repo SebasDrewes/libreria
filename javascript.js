9f6d5a3347839b0a6987b42009f417e0fbc4a9b5
Sebastian Drewes <sebas.drewes@gmail.com>
2021-05-16 12:21:18 -0300

    agregados estilos

--- javascript.js
+++ javascript.js
@@ -1,36 +1,27 @@
-const display = document.querySelector(".libreria")
+//definicion de elementos DOM
 const nuevoLibro = document.querySelector(".nuevoLibro")
-const tituloLibro = document.querySelector(".tituloLibro")
-const autor = document.querySelector(".autor")
-const paginas = document.querySelector(".paginas")
-const read = document.querySelector(".read")
 const libreria = document.querySelector(".libreria")
-const displayForm = document.querySelector(".displayForm")
 const newBok = document.querySelector(".newBook")
 const newAutor = document.querySelector(".newAuthor")
 const newPages = document.querySelector(".newPages")
 const newRead = document.querySelector(".newRead")
+
+//libros test
+const theHobbit = new Book("The Hobbit", "J.R.R Tolkien", 295, "Leido")
+const harryPotter = new Book("Harry Potter", "J.K Rollin", 255, "Leido")
+//definicion array de libros
+let myLibrary = [theHobbit, harryPotter];
+//mostrar libros actuales
+window.onload = displayBooks();
+
+//object constructor
 function Book(title, author, pages, read) {
     this.title = title 
     this.author = `de ${author}`
     this.pages = `${pages} paginas`
     this.read = read;
- /*   Book.prototype.changeRead = function() {
-        if (this.read === "Leido") {
-            this.read = "No Leido"
-        }else {
-            this.read = "Leido"
-        }
-    }
-    Book.prototype.remove = function() {
-        for (let i=0; i<myLibrary.length; i++) {
-            if (myLibrary[i] === this) {
-                myLibrary.splice(i, 1)
-            }
-        }
-        displayBooks()*/
 }
-
+//funcionalidad agregar libro a libreria
 function addBookToLibrary(){
     let newTitle = newBok.value
     let newAuthor = newAutor.value
@@ -51,24 +42,27 @@ function addBookToLibrary(){
     closeNav()
     }
 }
+//funcionalidad guardar libreria en localStorage
 function saveLibrary() {
 localStorage.setItem("myLibrary",JSON.stringify(myLibrary))
 }
+//funcionalidad cargar libreria de localStorage
 function loadLibrary() {
     let storedLibrary = localStorage.getItem("myLibrary");
     myLibrary = JSON.parse(storedLibrary);
 }
-
+//funcionalidad para filtrar libreria de nulls
 function removeNull(array) {
     return array.filter(x => x !== null)
     };
-
+//funcionalidad display libreria
 function displayBooks() {
     //borra todos los libros anteriores para hacer todos nuevamente
     loadLibrary()
     while (libreria.firstChild) {
         libreria.removeChild(libreria.firstChild);
     }
+    //creacion de elementos por cada libro
     for (let i=0; i<myLibrary.length; i++) {
         const libroAgregado= document.createElement('div');
         libroAgregado.classList.add("libroAgregado");
@@ -82,6 +76,7 @@ function displayBooks() {
         paginasAgregado.classList.add("paginasAgregado");
         paginasAgregado.textContent = myLibrary[i].pages;
         const readAgregado= document.createElement('div');
+        //funcionalidad cambiar valor leido/no leido
         readAgregado.classList.add("readAgregado");
         readAgregado.textContent = myLibrary[i].read;
         readAgregado.addEventListener('click', () => {
@@ -93,6 +88,7 @@ function displayBooks() {
             readAgregado.textContent = myLibrary[i].read;
             saveLibrary()
         })
+        //funcionalidad elimitar libro de libreria
         const removeAgregado= document.createElement('button');
         removeAgregado.classList.add("removeAgregado");
         removeAgregado.addEventListener('click', () => {
@@ -101,6 +97,7 @@ function displayBooks() {
                 saveLibrary()
         displayBooks()
         })
+        //construccion display
         libreria.appendChild(libroAgregado);
         libroAgregado.appendChild(tituloAgregado);
         libroAgregado.appendChild(autorAgregado);
@@ -109,30 +106,18 @@ function displayBooks() {
         libroAgregado.appendChild(removeAgregado);
     }
 }
-//https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_popup_form
-function form(){
-    const queryForm = document.createElement('div');
-    queryForm.classList.add("queryForm");
-    libreria.appendChild(queryForm)
-}
-const theHobbit = new Book("The Hobbit", "J.R.R Tolkien", 295, "Leido")
-const harryPotter = new Book("Harry Potter", "J.K Rollin", 255, "Leido")
-let myLibrary = [theHobbit, harryPotter];
-displayBooks();
-nuevoLibro.addEventListener('click', () => {
-    addBookToLibrary()
-})
-    
-
-
+//funcionalidad de pop up para agregar nuevo libro
 function openNav() {
     document.getElementById("myNav").style.height = "100%";
     newBok.value = "";
     newAutor.value = "";
     newPages.value = "";
     newRead.checked = false;
-  }
+}
   
-  function closeNav() {
+function closeNav() {
     document.getElementById("myNav").style.height = "0%";
-  }
+}
+nuevoLibro.addEventListener('click', () => {
+    addBookToLibrary()
+})
